17929f52a177f7d5aa5cb1796cef7add36deaa5d
OrionCo <wincenty.korobacz@gmail.com>
2020-02-19 16:56:49 +0100

    Added verification of the active status of an account and the display of related error message. modified:   authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -16,13 +16,13 @@
         header('location: landing-page.php');
     }
 
-    if($stmt = $con->prepare("SELECT u.`User_id`, `Hashed_pass`, `Is_admin` FROM users u LEFT JOIN user_level ul ON u.User_id = ul.User_id WHERE `Username` = ?")){
+    if($stmt = $con->prepare("SELECT u.`User_id`, `Hashed_pass`, `Is_admin`, `Is_active` FROM users u LEFT JOIN user_level ul ON u.User_id = ul.User_id WHERE `Username` = ?")){
         $stmt->bind_param('s', $_POST['username']);
         $stmt->execute();
         $stmt->store_result();
 
         if($stmt->num_rows > 0){
-            $stmt->bind_result($id, $password, $isadmin);
+            $stmt->bind_result($id, $password, $isadmin, $isactive);
             $stmt->fetch();
             
             if(password_verify($_POST['password'], $password)){
@@ -31,12 +31,18 @@
                 $_SESSION['name'] = $_POST['username'];
                 $_SESSION['id'] = $id;
                 $_SESSION['isadmin'] = $isadmin;
-                
-                if($isadmin == 1){
-                    header('Location: admin-panel.php');
+
+                if($isactive == 0){
+                    $_SESSION['error'] = "Your account has been disabled.";
+                    header('Location: landing-page.php');
                 } else {
-                    header('Location: user-panel.php');
+                    if($isadmin == 1){
+                        header('Location: admin-panel.php');
+                    } else {
+                        header('Location: user-panel.php');
+                    }
                 }
+
             } else {
                 $_SESSION['error'] = "Username doesn't exist or the provided password is incorrect.";
                 header("location: landing-page.php");
